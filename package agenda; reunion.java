64fda0d80427d1db083c828424a7fd831bb9dc07
joaquin-munoz <joaquin.munoz.c01@mail.pucv.cl>
2025-09-27 15:08:01 -0300

    Update package agenda; reunion.java
    inicializacion de datos reunion

--- package agenda; reunion.java
+++ package agenda; reunion.java
@@ -1,35 +1,34 @@
 package agenda;
 
-
 public class Reunion {
-    private String fecha;   // formato: dd/mm/yyyy
+    private int id;
+    private String usuario;
+    private String fecha;
     private String hora;
-    private String descripcion;
     private String etiqueta;
 
-    public Reunion(String fecha, String hora, String descripcion, String etiqueta) {
+    public Reunion(int id, String usuario, String fecha, String hora, String etiqueta) {
+        this.id = id;
+        this.usuario = usuario;
         this.fecha = fecha;
         this.hora = hora;
-        this.descripcion = descripcion;
         this.etiqueta = etiqueta;
     }
 
-    // Getters y setters
+    public String getUsuario() { return usuario; }
+    public int getId() { return id; }
+    public void setUsuario(String usuario) { this.usuario = usuario; }
     public String getFecha() { return fecha; }
     public void setFecha(String fecha) { this.fecha = fecha; }
-
     public String getHora() { return hora; }
     public void setHora(String hora) { this.hora = hora; }
-
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-
     public String getEtiqueta() { return etiqueta; }
     public void setEtiqueta(String etiqueta) { this.etiqueta = etiqueta; }
 
     @Override
     public String toString() {
-        return fecha + " " + hora + " - " + descripcion + " [" + etiqueta + "]";
+        return "ID: " + id + " | [" + usuario + "] " + fecha + " " + hora + " [" + etiqueta + "]";
     }
 }
+
 
